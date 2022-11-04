96f329dcad16b13c8cf388b9ff68c2103633ba06
yash sajwan <yashsajwan12345@gmail.com>
2022-11-05 02:02:13 +0530

    dice game

---
+++ hello.js
@@ -0,0 +1,20 @@
+var numberOne = Math.floor(Math.random()*6)+1;
+var numberTwo = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "./dices/dice" + numberOne + ".png";
+var randomDiceImage2 = "./dices/dice" + numberTwo + ".png";
+
+document.querySelector(".dice1 img").setAttribute("src", randomDiceImage1);
+document.querySelector(".dice2 img").setAttribute("src", randomDiceImage2);
+
+if( numberOne > numberTwo ){
+    document.querySelector(".title").innerHTML = "🚩Player 1 is the Winner!";
+}
+else if( numberOne < numberTwo){
+    document.querySelector(".title").innerHTML = "🚩Player 2 is the Winner!";
+}
+else{
+    document.querySelector(".title").innerHTML = "🚩It's a Draw! ";
+}
+
+
