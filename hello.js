742288a81eb01c584f6e55068c5bcd8976654b87
yash sajwan <yashsajwan12345@gmail.com>
2022-11-05 03:59:47 +0530

    play button added

--- hello.js
+++ hello.js
@@ -1,20 +1,27 @@
-var numberOne = Math.floor(Math.random()*6)+1;
-var numberTwo = Math.floor(Math.random()*6)+1;
+function handleClick(){
 
-var randomDiceImage1 = "./dices/dice" + numberOne + ".png";
-var randomDiceImage2 = "./dices/dice" + numberTwo + ".png";
+    var numberOne = Math.floor(Math.random()*6)+1;
+    var numberTwo = Math.floor(Math.random()*6)+1;
 
-document.querySelector(".dice1 img").setAttribute("src", randomDiceImage1);
-document.querySelector(".dice2 img").setAttribute("src", randomDiceImage2);
+    var randomDiceImage1 = "./dices/dice" + numberOne + ".png";
+    var randomDiceImage2 = "./dices/dice" + numberTwo + ".png";
 
-if( numberOne > numberTwo ){
-    document.querySelector(".title").innerHTML = "🚩Player 1 is the Winner!";
+    document.querySelector(".dice1 img").setAttribute("src", randomDiceImage1);
+    document.querySelector(".dice2 img").setAttribute("src", randomDiceImage2);
+
+    var audio = new Audio("./sound/dj.mp3");
+    audio.play();
+
+    if( numberOne > numberTwo ){
+        document.querySelector(".title").innerHTML = "🚩Player 1 is the Winner!";
+    }
+    else if( numberOne < numberTwo){
+        document.querySelector(".title").innerHTML = "🚩Player 2 is the Winner!";
+    }
+    else{
+        document.querySelector(".title").innerHTML = "🚩It's a Draw! ";
+    }   
 }
-else if( numberOne < numberTwo){
-    document.querySelector(".title").innerHTML = "🚩Player 2 is the Winner!";
-}
-else{
-    document.querySelector(".title").innerHTML = "🚩It's a Draw! ";
-}
 
 
+document.querySelector(".button").addEventListener("click", handleClick);
