b8126a0693d1981651cf8d9f2accdf161d3ac9bf
yash sajwan <yashsajwan12345@gmail.com>
2022-12-17 12:24:23 +0530

    first commit

--- hello.js
+++ hello.js
@@ -1,25 +1,34 @@
-for(var i = 0; i < document.querySelectorAll(".button").length ; i++){
-    document.querySelectorAll(".button")[i].addEventListener("click", function(){
-    var number = this.innerHTML;
-    animation(this);
-    var audio = new Audio("./sound/beep"+number+".mp3");
-    audio.play();
-    });
-}
-
-document.addEventListener("keypress", function(event){
-    var number = event.key;
-    if(number >=1 && number<=6){
-        var audio = new Audio("./sound/beep"+number+".mp3");
-        audio.play();
-        var key=document.querySelectorAll(".button")[number-1];
-        animation(key);
+let container = document.querySelector(".container");
+let x= 1;
+container.addEventListener("click", function () {
+    // to prevent the addition of textarea again and again 
+    let noTextArea = document.getElementsByClassName("textarea");
+    if (noTextArea.length == 0) {
+        container.innerHTML = `<textarea class="textarea" id="textarea" rows='25' cols='70' placeholder="Write something" ></textarea>`;
+        document.getElementById("textarea").focus();
     }
-});
+    let textarea = document.getElementById("textarea");
+    textarea.addEventListener("blur", function () {
+        if(textarea.value==0){
+            container.innerHTML = " Click to write Something";
+        }
+        else{
+            container.innerHTML = textarea.value;   
+        }
+    })
 
-function animation(currentkey){
-    currentkey.classList.add("pressed");
-    setTimeout(function(){
-        currentkey.classList.remove("pressed");
-    }, 100); 
-}
+    if(x==1){
+    let divEle = document.createElement("div");
+    let text = document.createTextNode("hello world!");
+    divEle.appendChild(text);
+    divEle.setAttribute("style", "color: white;");
+    let first = document.querySelector("h1");
+    first.appendChild(divEle);
+    }
+    x=0;
+
+
+
+
+})
+
